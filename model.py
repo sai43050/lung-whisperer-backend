34244e65791be11280935835f2cc38edcf5e1a48
sai43050 <sudulaguntasrisai@gmail.com>
2026-03-02 20:57:34 +0530

    corrected LSTM input size to 640

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ class LungWhispererModel(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
         self.pool = nn.MaxPool2d(2,2)
-        self.lstm = nn.LSTM(64, 128, batch_first=True, bidirectional=True)
+        self.lstm = nn.LSTM(640, 128, batch_first=True, bidirectional=True)
         self.attention = Attention(256)
         self.dropout = nn.Dropout(0.3)
         self.fc = nn.Linear(256, num_classes)
